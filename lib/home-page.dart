45a0d1dec366c0e025be5a0338b1e4331553cdfa
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-09-03 01:03:55 +0100

    feat add dynamisation getting list ville

--- lib/home-page.dart
+++ lib/home-page.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<dynamic> listTowns;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,8 +17,35 @@ class HomePage extends StatelessWidget {
         title: Text('Home Page'),
       ),
       body: Center(
-        child: Text("Bienvenue a la page d'accueil"),
+        child: this.listTowns == null
+            ? CircularProgressIndicator()
+            : ListView.builder(
+                itemCount: this.listTowns == null ? 0 : this.listTowns.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Colors.deepOrange,
+                    child: Text(this.listTowns[index]['name']),
+                  );
+                }),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadTowns();
+  }
+
+  void loadTowns() {
+    String url = 'http://192.168.43.74/fluuter';
+
+    http.get(Uri.parse(url)).then((resp) {
+      setState(() {
+        print(resp);
+        this.listTowns = json.decode(resp.body)['results'];
+      });
+    });
   }
 }
